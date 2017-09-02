227b3eb4c8accbd695ba57ab3122d23e767f7732
peidi19870822 <31541549+peidi19870822@users.noreply.github.com>
2017-09-02 15:24:39 +0800

    Update MavenFirst.java

--- MavenFirst/MavenFirst/src/main/java/com/itheima/maven/MavenFirst.java
+++ MavenFirst/MavenFirst/src/main/java/com/itheima/maven/MavenFirst.java
@@ -20,7 +20,7 @@ public class MavenFirst {
 		name=+"pei";
 		return "hello " + name;
 	}
-	public String sayHello2(String name) {
+	public String sayHello3(String name) {
 		return "hello " + name;
 	}
 }
