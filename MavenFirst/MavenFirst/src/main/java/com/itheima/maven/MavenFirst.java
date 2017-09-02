e41f54afecc86f11521a13667bee1db7de1dd1a5
peidi19870822 <peidi19870822@126.com>
2017-09-02 14:31:48 +0800

    修改注释

--- MavenFirst/MavenFirst/src/main/java/com/itheima/maven/MavenFirst.java
+++ MavenFirst/MavenFirst/src/main/java/com/itheima/maven/MavenFirst.java
@@ -1,4 +1,4 @@
-package com.itheima.maven;
+﻿package com.itheima.maven;
 
 /**
  *  
@@ -12,7 +12,7 @@ package com.itheima.maven;
  * <p>
  * Company: www.itcast.com
  * </p>
- *  @author 传智.关云长   @date 2015-12-20 上午10:36:40    @version 1.0
+ *  @author 佩蒂   @date 2015-12-20 上午10:36:40    @version 1.0
  */
 public class MavenFirst {
 
